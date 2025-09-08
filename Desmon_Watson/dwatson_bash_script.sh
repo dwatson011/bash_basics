d5f92e85dae4c396becc5a1ba75c1a16a0948dbf
Desmon Watson <watson.desmon16@gmail.com>
2025-09-07 21:23:54 -0400

    add dwatson bash script

---
+++ Desmon_Watson/dwatson_bash_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Hello! Welcome to Desmon's first Bash script!"
+
+echo "What is your name?"
+read name
+
+echo "How old are you?"
+read age
+
+echo ""
+echo "Nice to meet you, $name!"
+echo "You are $age years old."
+
+echo "*************"
+echo "** $name **"
+echo "*************"
