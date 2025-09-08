e48dc0b07d14e1c32e05d312671ec005fbcf2ef8
Desmon Watson <watson.desmon16@gmail.com>
2025-09-07 22:43:29 -0400

    Alt ver: added D and F logic to 05-grade.sh

--- 05-grade.sh
+++ 05-grade.sh
@@ -11,9 +11,12 @@ elif [ $grade -ge 80 ]; then
    echo "You got a B. That's good."
 elif [ $grade -ge 70 ]; then
    echo "You got a C. Not bad."
+elif [ $grade -ge 60 ]; then
+   echo "You got a D. You passed, but there's room to improve."
 else
-   echo "Time to work on some extra credit assignments"
+   echo "You got an F. Looks like it's time for some extra credit."
 fi
+
 
 # exercise: write a script (that utilizes weather-util) 
 # that prints "it's cold" if the temperature is < 40
